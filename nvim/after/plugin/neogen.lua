504df2c77fd4516aeba606068cdc7d3c469460ac
Alex98235 <129420303+Alex98235@users.noreply.github.com>
2024-10-19 13:07:51 +0200

    Delete nvim/after/plugin directory

--- nvim/after/plugin/neogen.lua
+++
@@ -1,8 +0,0 @@
-local opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap("n", "<Leader>nf", ":lua require('neogen').generate()<CR>", opts)
-
-require('neogen').setup({
-    languages = {
-        ['cpp.doxygen'] = require('neogen.configurations.cpp')
-    }
-})
